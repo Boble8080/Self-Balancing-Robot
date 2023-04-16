754c6b66f610873e55c5ccfa9ef5c313fb5226b0
Boble8080 <95143674+Boble8080@users.noreply.github.com>
2023-04-16 23:58:32 +0100

    PID

--- StationaryWithMotors/StationaryWithMotors.ino
+++ StationaryWithMotors/StationaryWithMotors.ino
@@ -2,6 +2,8 @@
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
 #include "Wire.h"
+#include <PID_v1.h>
+#include "Math.h"
 
 #define MpuInterruptPin 23
 
@@ -21,121 +23,114 @@
 
 #define potPin 34
 
-#define pwmHz 1000 // PWM frequency of 1 KHz // test others
-#define pwmRes 8 // 8-bit resolution
+#define pwmHz 1000  // PWM frequency of 1 KHz // test others
+#define pwmRes 8    // 8-bit resolution
+
 
 /////////////////////////////////////////////////////////////////////////
 //                          Motor Class                                //
 /////////////////////////////////////////////////////////////////////////
+int motorGain;
+class Motor {
+public:
+  byte Pin1;
+  byte Pin2;
+  byte PinPWM;
+  byte PWMchannel;
 
-class Motor
-{
-  public:
-    byte Pin1;
-    byte Pin2;
-    byte PinPWM;
-    byte PWMchannel;
-
-    Motor(byte Pin1, byte Pin2, byte pinPWM, byte PWMchannel)
-    {
-      // Stores constructor input as private variables
-      this->Pin1 = Pin1;
-      this->Pin2 = Pin2;
-      this->PinPWM = PinPWM;
-      this->PWMchannel = PWMchannel;
-      ledcSetup(PWMchannel, pwmHz, pwmRes);
-      ledcAttachPin(pinPWM, PWMchannel);
-      pinMode(Pin1, OUTPUT);
-      pinMode(Pin2, OUTPUT);
+  Motor(byte Pin1, byte Pin2, byte pinPWM, byte PWMchannel) {
+    // Stores constructor input as private variables
+    this->Pin1 = Pin1;
+    this->Pin2 = Pin2;
+    this->PinPWM = PinPWM;
+    this->PWMchannel = PWMchannel;
+    ledcSetup(PWMchannel, pwmHz, pwmRes);
+    ledcAttachPin(pinPWM, PWMchannel);
+    pinMode(Pin1, OUTPUT);
+    pinMode(Pin2, OUTPUT);
+  }
+  void rotate(int speed) {
+    //motorGain = 20;
+    //Serial.println(speed);
+    if (speed == 0) {
+      digitalWrite(Pin1, 0);
+      digitalWrite(Pin2, 0);
+      ledcWrite(PWMchannel, 0);
+      //Serial.print("\t");
+      //Serial.print(abs(speed));
+    } else if (speed > 0) {
+      digitalWrite(Pin1, 1);
+      digitalWrite(Pin2, 0);
+      ledcWrite(PWMchannel, speed + motorGain);
+      //Serial.print("\t");
+      //Serial.print(speed + motorGain);
+    } else if (speed < 0) {
+      digitalWrite(Pin1, 0);
+      digitalWrite(Pin2, 1);
+      ledcWrite(PWMchannel, abs(speed) + motorGain);
+      //Serial.print("\t");
+      //Serial.print(abs(speed) + motorGain);
     }
-    void rotate(int speed)
-    {
-      Serial.println(speed);
-      if (speed == 0)
-      {
-        digitalWrite(Pin1, 0);
-        digitalWrite(Pin2, 0);
-        ledcWrite(PWMchannel, 0);
-        Serial.println("stop");
-      }
-      else if (speed > 0)
-      {
-        digitalWrite(Pin1, 1);
-        digitalWrite(Pin2, 0);
-        ledcWrite(PWMchannel, speed);
-        Serial.println("forward");
-      }
-      else if (speed < 0)
-      {
-        digitalWrite(Pin1, 0);
-        digitalWrite(Pin2, 1);
-        ledcWrite(PWMchannel, 257 - speed);
-        Serial.println("back");
-      }
-    }
+  }
 };
+
+Motor leftMotor(Lmotor1, Lmotor2, LmotorEn, LpwmChannel);
+Motor rightMotor(Rmotor1, Rmotor2, RmotorEn, RpwmChannel);
 
 /////////////////////////////////////////////////////////////////////////
 //                          Encoder                                    //
 /////////////////////////////////////////////////////////////////////////
 
-const byte        LeftEncoderInterrupt = 36;          // Assign the interrupt pin
-volatile uint64_t LeftStartValue = 0;                 // First interrupt value
-volatile uint64_t LeftPeriodCount;                    // period in counts
-float             LeftFreq;                           // frequency
-float             LeftRPM;
-hw_timer_t *      LeftTimer = NULL;                        // pointer to a variable of type hw_timer_t
+const byte LeftEncoderInterrupt = 36;  // Assign the interrupt pin
+volatile uint64_t LeftStartValue = 0;  // First interrupt value
+volatile uint64_t LeftPeriodCount;     // period in counts
+float LeftFreq;                        // frequency
+float LeftRPM;
+hw_timer_t* LeftTimer = NULL;  // pointer to a variable of type hw_timer_t
 
 
-const byte        RightEncoderInterrupt = 39;          // Assign the interrupt pin
-volatile uint64_t RightStartValue = 0;                 // First interrupt value
-volatile uint64_t RightPeriodCount;                    // period in counts
-float             RightFreq;                           // frequency
-float             RightRPM;
-hw_timer_t *      RightTimer = NULL;                        // pointer to a variable of type hw_timer_t
+const byte RightEncoderInterrupt = 39;  // Assign the interrupt pin
+volatile uint64_t RightStartValue = 0;  // First interrupt value
+volatile uint64_t RightPeriodCount;     // period in counts
+float RightFreq;                        // frequency
+float RightRPM;
+hw_timer_t* RightTimer = NULL;  // pointer to a variable of type hw_timer_t
 
-void IRAM_ATTR handleLeftInterrupt()
-{
-  uint64_t TempVal = timerRead(LeftTimer);            // value of timer at interrupt
-  LeftPeriodCount = TempVal - LeftStartValue;             // period count between rising edges
-  LeftStartValue = TempVal;                           // puts latest reading as start for next calculation
+void IRAM_ATTR handleLeftInterrupt() {
+  uint64_t TempVal = timerRead(LeftTimer);     // value of timer at interrupt
+  LeftPeriodCount = TempVal - LeftStartValue;  // period count between rising edges
+  LeftStartValue = TempVal;                    // puts latest reading as start for next calculation
 }
 
-void IRAM_ATTR handleRightInterrupt()
-{
-  uint64_t TempVal = timerRead(RightTimer);            // value of timer at interrupt
-  RightPeriodCount = TempVal - RightStartValue;             // period count between rising edges
-  RightStartValue = TempVal;                           // puts latest reading as start for next calculation
+void IRAM_ATTR handleRightInterrupt() {
+  uint64_t TempVal = timerRead(RightTimer);      // value of timer at interrupt
+  RightPeriodCount = TempVal - RightStartValue;  // period count between rising edges
+  RightStartValue = TempVal;                     // puts latest reading as start for next calculation
 }
 
-float LeftFrequency()
-{
+float LeftFrequency() {
   LeftFreq = 40000000.00 / LeftPeriodCount;
   return RightFreq;
 }
 
-float RightFrequency()
-{
+float RightFrequency() {
   RightFreq = 40000000.00 / RightPeriodCount;
   return RightFreq;
 }
 
-float returnLeftRPM()
-{
-  LeftFreq = 40000000.00 / LeftPeriodCount;// calculate frequency
+float returnLeftRPM() {
+  LeftFreq = 40000000.00 / LeftPeriodCount;  // calculate frequency
   LeftRPM = (LeftFreq * 6.0) / 49.0;
   return LeftRPM;
 }
 
-float returnRightRPM()
-{
-  RightFreq = 40000000.00 / RightPeriodCount;// calculate frequency
+float returnRightRPM() {
+  RightFreq = 40000000.00 / RightPeriodCount;  // calculate frequency
   RightRPM = (RightFreq * 6.0) / 49.0;
   return RightRPM;
 }
 
-Motor leftMotor(Lmotor1, Lmotor2, LmotorEn, LpwmChannel);
-Motor rightMotor(Rmotor1, Rmotor2, RmotorEn, RpwmChannel);
+
 
 /////////////////////////////////////////////////////////////////////////
 //                          Gyroscope                                  //
@@ -143,40 +138,34 @@ Motor rightMotor(Rmotor1, Rmotor2, RmotorEn, RpwmChannel);
 
 MPU6050 mpu;
 
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
+bool dmpReady = false;   // set true if DMP init was successful
+uint8_t mpuIntStatus;    // holds actual interrupt status byte from MPU
+uint8_t devStatus;       // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;     // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;      // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64];  // FIFO storage buffer
 
 // orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+Quaternion q;         // [w, x, y, z]         quaternion container
+VectorInt16 aa;       // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;   // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;  // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;  // [x, y, z]            gravity vector
+float euler[3];       // [psi, theta, phi]    Euler angle container
+float ypr[3];         // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+volatile bool mpuInterrupt = false;  // indicates whether MPU interrupt pin has gone high
 void dmpDataReady() {
   mpuInterrupt = true;
 }
 
 void debugGyro() {
-  Serial.print(-90);
-  Serial.print(" ");
-  Serial.print(90);
-  Serial.print(" ");
-  Serial.print(0);
-  Serial.print("\t");
   Serial.print("Gyro:ypr\t");
   Serial.print(ypr[0] * 180 / M_PI);
   Serial.print("\t");
   Serial.print(ypr[1] * 180 / M_PI);
   Serial.print("\t");
-  Serial.println(ypr[2] * 180 / M_PI);
+  Serial.print(ypr[2] * 180 / M_PI);
 }
 
 void debugAccel() {
@@ -189,7 +178,7 @@ void debugAccel() {
   Serial.print("\t");
   Serial.print(aaWorld.y);
   Serial.print("\t");
-  Serial.println(aaWorld.z);
+  Serial.print(aaWorld.z);
 }
 
 void debugBoth() {
@@ -204,17 +193,41 @@ void debugBoth() {
   Serial.print("\tPch: ");
   Serial.print(ypr[1] * 180 / M_PI);
   Serial.print("\tRll: ");
-  Serial.println(ypr[2] * 180 / M_PI);
+  Serial.print(ypr[2] * 180 / M_PI);
+}
+void readMPU() {
+  if (!dmpReady) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    return;
+  }
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) {
+
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetAccel(&aa, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+    mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 }
 
 /////////////////////////////////////////////////////////////////////////
 //                          Setup & Loop                               //
 /////////////////////////////////////////////////////////////////////////
 
-void setup()
-{
+double Setpoint = -5;
+double Input, Output;
+double Kp = 5.7, Ki = 0.1, Kd = 0.2;
+
+
+PID balancePID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+
+void setup() {
+  motorGain = 18;
   Wire.begin();
-  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+  Wire.setClock(400000);  // 400kHz I2C clock. Comment this line if having compilation difficulties
   Serial.begin(115200);
   mpu.initialize();
   pinMode(MpuInterruptPin, INPUT);
@@ -224,9 +237,45 @@ void setup()
   mpu.setXGyroOffset(127);
   mpu.setYGyroOffset(-101);
   mpu.setZGyroOffset(15);
-  mpu.setXAccelOffset(-409); 
-  mpu.setYAccelOffset(409); 
-  mpu.setZAccelOffset(1331); 
+  mpu.setXAccelOffset(-409);
+  mpu.setYAccelOffset(409);
+  mpu.setZAccelOffset(1331);
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // Calibration Time: generate offsets and calibrate our MPU6050
+    //mpu.CalibrateAccel(6);
+    //mpu.CalibrateGyro(6);
+    mpu.PrintActiveOffsets();
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+    Serial.print(digitalPinToInterrupt(MpuInterruptPin));
+    Serial.println(F(")..."));
+    attachInterrupt(digitalPinToInterrupt(MpuInterruptPin), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
+
+  // configure LED for output
+  pinMode(LED_BUILTIN, OUTPUT);
 
   pinMode(Lencoder, INPUT);
   attachInterrupt(Lencoder, handleLeftInterrupt, FALLING);
@@ -239,17 +288,30 @@ void setup()
   timerStart(LeftTimer);
 
   analogReadResolution(9);
+
+  balancePID.SetMode(AUTOMATIC);
+  balancePID.SetOutputLimits(-200, 200);
 }
 
-int val = 0;
 
-void loop()
-{
-  val = analogRead(potPin) / 2;
-  rightMotor.rotate(val);
-  leftMotor.rotate(val);
-  delay(1000);
-  rightMotor.rotate(-val);
-  leftMotor.rotate(-val);
-  delay(1000);
+void loop() {
+  // Kd = analogRead(potPin)/100.0;
+  // Serial.print(Kd);
+  // Serial.print("\t");
+  // balancePID.SetTunings(Kp, Ki, Kd);
+  readMPU();
+  Input = -ypr[1] * 180 / M_PI;
+  if (Input >= 50 || Input <= -50) {
+    while (1) {
+      leftMotor.rotate(0);
+      rightMotor.rotate(0);
+      
+    }
+  }
+  balancePID.Compute();
+  debugGyro();
+  Serial.print("\t");
+  Serial.println(Output);
+  leftMotor.rotate(Output);
+  rightMotor.rotate(Output);
 }
