54772ee78135bd67c1c3a953e55ccb073cade391
Boble8080 <jakubpytlak284@gmail.com>
2023-04-20 17:14:07 +0100

    kicad done and pid integrated

--- StationaryWithMotors/StationaryWithMotors.ino
+++ StationaryWithMotors/StationaryWithMotors.ino
@@ -2,8 +2,8 @@
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
 #include "Wire.h"
-#include <PID_v1.h>
-#include "Math.h"
+//#include <PID_v1.h>
+#include "math.h"
 
 #define MpuInterruptPin 23
 
@@ -26,52 +26,178 @@
 #define pwmHz 1000  // PWM frequency of 1 KHz // test others
 #define pwmRes 8    // 8-bit resolution
 
+//////////////////////////////////////////////
+//        RemoteXY include library          //
+//////////////////////////////////////////////
+
+// RemoteXY select connection mode and include library 
+#define REMOTEXY_MODE__ESP32CORE_BLE
+#include <BLEDevice.h>
+
+#include <RemoteXY.h>
+
+// RemoteXY connection settings 
+#define REMOTEXY_BLUETOOTH_NAME "SelfBalancingRobot"
+
+
+// RemoteXY configurate  
+#pragma pack(push, 1)
+uint8_t RemoteXY_CONF[] =   // 72 bytes
+  { 255,2,0,15,0,65,0,16,170,2,5,0,66,30,30,30,17,62,30,30,
+  2,26,31,71,56,70,4,25,25,5,1,54,54,0,2,24,75,0,0,180,
+  194,0,0,180,66,0,0,240,65,0,0,32,65,0,0,160,64,24,0,67,
+  4,12,47,20,5,2,39,60,8,2,26,11 };
+  
+// this structure defines all the variables and events of your control interface 
+struct {
+
+    // input variables
+  int8_t joystick_x; // from -100 to 100  
+  int8_t joystick_y; // from -100 to 100  
+
+    // output variables
+  float Angle;  // from -90 to 90 
+  char textBox[11];  // string UTF8 end zero 
+
+    // other variable
+  uint8_t connect_flag;  // =1 if wire connected, else =0 
+
+} RemoteXY;
+#pragma pack(pop)
 
 /////////////////////////////////////////////////////////////////////////
-//                          Motor Class                                //
+//                              PID                                    //
+/////////////////////////////////////////////////////////////////////////
+class PID
+{
+  public:
+
+    double kp;                  // * (P)roportional Tuning Parameter
+    double ki;                  // * (I)ntegral Tuning Parameter
+    double kd;                  // * (D)erivative Tuning Parameter
+
+    double *myInput;              // * Pointers to the Input, Output, and Setpoint variables
+    double *myOutput;             //   This creates a hard link between the variables and the
+    double *mySetpoint;           //   PID, freeing the user from having to constantly tell us
+
+    unsigned long lastTime;
+    double outputSum, lastInput;
+
+    unsigned long SampleTime = 4;
+    double outMin = -255;
+    double outMax =  255;
+    bool inAuto;
+
+    
+    PID(double* Input, double* Output, double* Setpoint, double Kp, double Ki, double Kd)
+    {
+      myOutput = Output;
+      myInput = Input;
+      mySetpoint = Setpoint;
+      inAuto = false;
+      PID::SetTunings(Kp, Ki, Kd);
+      lastTime = millis() - SampleTime;
+    }
+
+    void SetMode(int Mode)
+    {
+      bool newAuto = (Mode == 1);
+      if (newAuto && !inAuto)
+      { /*we just went from manual to auto*/
+        outputSum = *myOutput;
+        lastInput = *myInput;
+        if (outputSum > outMax) outputSum = outMax;
+        else if (outputSum < outMin) outputSum = outMin;
+      }
+      inAuto = newAuto;
+    }
+    bool Compute()
+    {
+      if (!inAuto) return false;
+      unsigned long now = millis();
+      unsigned long timeChange = (now - lastTime);
+      if (timeChange >= SampleTime)
+      {
+        /*Compute all the working error variables*/
+        double input = *myInput;
+        double error = *mySetpoint - input;
+        double dInput = (input - lastInput);
+        outputSum += (ki * error);
+        
+        if (outputSum > outMax) outputSum = outMax;
+        else if (outputSum < outMin) outputSum = outMin;
+
+        double output;
+        output = kp * error;
+
+
+        /*Compute Rest of PID Output*/
+        output += outputSum - kd * dInput;
+
+        if (output > outMax) output = outMax;
+        else if (output < outMin) output = outMin;
+        *myOutput = output;
+
+        /*Remember some variables for next time*/
+        lastInput = input;
+        lastTime = now;
+        return true;
+      }
+      else return false;
+    }
+    void SetTunings(double Kp, double Ki, double Kd)
+    {
+      double SampleTimeInSec = ((double)SampleTime) / 1000;
+      kp = Kp;
+      ki = Ki * SampleTimeInSec;
+      kd = Kd / SampleTimeInSec;
+    }
+};
+/////////////////////////////////////////////////////////////////////////
+//                               Motor                                 //
 /////////////////////////////////////////////////////////////////////////
 int motorGain;
 class Motor {
-public:
-  byte Pin1;
-  byte Pin2;
-  byte PinPWM;
-  byte PWMchannel;
+  public:
+    byte Pin1;
+    byte Pin2;
+    byte PinPWM;
+    byte PWMchannel;
 
-  Motor(byte Pin1, byte Pin2, byte pinPWM, byte PWMchannel) {
-    // Stores constructor input as private variables
-    this->Pin1 = Pin1;
-    this->Pin2 = Pin2;
-    this->PinPWM = PinPWM;
-    this->PWMchannel = PWMchannel;
-    ledcSetup(PWMchannel, pwmHz, pwmRes);
-    ledcAttachPin(pinPWM, PWMchannel);
-    pinMode(Pin1, OUTPUT);
-    pinMode(Pin2, OUTPUT);
-  }
-  void rotate(int speed) {
-    //motorGain = 20;
-    //Serial.println(speed);
-    if (speed == 0) {
-      digitalWrite(Pin1, 0);
-      digitalWrite(Pin2, 0);
-      ledcWrite(PWMchannel, 0);
-      //Serial.print("\t");
-      //Serial.print(abs(speed));
-    } else if (speed > 0) {
-      digitalWrite(Pin1, 1);
-      digitalWrite(Pin2, 0);
-      ledcWrite(PWMchannel, speed + motorGain);
-      //Serial.print("\t");
-      //Serial.print(speed + motorGain);
-    } else if (speed < 0) {
-      digitalWrite(Pin1, 0);
-      digitalWrite(Pin2, 1);
-      ledcWrite(PWMchannel, abs(speed) + motorGain);
-      //Serial.print("\t");
-      //Serial.print(abs(speed) + motorGain);
+    Motor(byte inPin1, byte inPin2, byte inPinPWM, byte inPWMchannel) {
+      // Stores constructor input as private variables
+      Pin1 = inPin1;
+      Pin2 = inPin2;
+      PinPWM = inPinPWM;
+      PWMchannel = inPWMchannel;
+      ledcSetup(PWMchannel, pwmHz, pwmRes);
+      ledcAttachPin(PinPWM, PWMchannel);
+      pinMode(Pin1, OUTPUT);
+      pinMode(Pin2, OUTPUT);
     }
-  }
+    void rotate(int speed) {
+      //motorGain = 20;
+      //Serial.println(speed);
+      if (speed == 0) {
+        digitalWrite(Pin1, 0);
+        digitalWrite(Pin2, 0);
+        ledcWrite(PWMchannel, 0);
+        //Serial.print("\t");
+        //Serial.print(abs(speed));
+      } else if (speed > 0) {
+        digitalWrite(Pin1, 1);
+        digitalWrite(Pin2, 0);
+        ledcWrite(PWMchannel, speed + motorGain);
+        //Serial.print("\t");
+        //Serial.print(speed + motorGain);
+      } else if (speed < 0) {
+        digitalWrite(Pin1, 0);
+        digitalWrite(Pin2, 1);
+        ledcWrite(PWMchannel, abs(speed) + motorGain);
+        //Serial.print("\t");
+        //Serial.print(abs(speed) + motorGain);
+      }
+    }
 };
 
 Motor leftMotor(Lmotor1, Lmotor2, LmotorEn, LpwmChannel);
@@ -221,10 +347,11 @@ double Input, Output;
 double Kp = 5.7, Ki = 0.1, Kd = 0.2;
 
 
-PID balancePID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+PID balancePID(&Input, &Output, &Setpoint, Kp, Ki, Kd);
 
 
 void setup() {
+  RemoteXY_Init(); 
   motorGain = 18;
   Wire.begin();
   Wire.setClock(400000);  // 400kHz I2C clock. Comment this line if having compilation difficulties
@@ -243,9 +370,6 @@ void setup() {
 
   // make sure it worked (returns 0 if so)
   if (devStatus == 0) {
-    // Calibration Time: generate offsets and calibrate our MPU6050
-    //mpu.CalibrateAccel(6);
-    //mpu.CalibrateGyro(6);
     mpu.PrintActiveOffsets();
     // turn on the DMP, now that it's ready
     Serial.println(F("Enabling DMP..."));
@@ -289,12 +413,12 @@ void setup() {
 
   analogReadResolution(9);
 
-  balancePID.SetMode(AUTOMATIC);
-  balancePID.SetOutputLimits(-200, 200);
+  balancePID.SetMode(1);
 }
 
 
 void loop() {
+  //RemoteXY_Handler();
   // Kd = analogRead(potPin)/100.0;
   // Serial.print(Kd);
   // Serial.print("\t");
@@ -305,7 +429,7 @@ void loop() {
     while (1) {
       leftMotor.rotate(0);
       rightMotor.rotate(0);
-      
+
     }
   }
   balancePID.Compute();
